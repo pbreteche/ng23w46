666255086a453d86595aca04194ba07e5c5ab0b4
Pierre Bretéché <pbreteche@dawan.fr>
2023-11-16 09:44:20 +0100

    client HTTP

--- src/app/song-list.service.ts
+++ src/app/song-list.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
 import {songs} from "./fixtures/songs";
 import {Song} from "./model/song";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SongListService {
-  songs = songs;
+  songs: Map<number,Song> = new Map();
   nextId = 34;
+
+  constructor(private client: HttpClient) {
+    this.client.get('/assets/songs.json').subscribe(
+      (data) => {
+        if (!Array.isArray(data)) {
+          return;
+        }
+        data.forEach((song => this.songs.set(song.id, song)))
+      }
+    )
+  }
 
   add(song: Song) {
     song.id = this.nextId;
