bb2a533a621f4b9acbc471cbace35edafd878153
Pierre Bretéché <pbreteche@dawan.fr>
2023-11-16 12:09:15 +0100

    HTTP interceptor (écriture actuelle)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withInterceptors} from "@angular/common/http";
+import {tokenInterceptor} from "./token.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
   ]
 };
---
+++ src/app/token.interceptor.ts
@@ -0,0 +1,9 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import {inject} from "@angular/core";
+import {TokenService} from "./token.service";
+
+export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const tokenReq = req.clone({ setHeaders: {Authorization: inject(TokenService).token}})
+
+  return next(tokenReq);
+};
